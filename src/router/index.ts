25ab41ae9e42b76a1d4a11bd26d3059b10aaf1ad
tomas <Tomascn777@gmail.com>
2026-09-22 20:34:30 +0200

    Configura despliegue y rutas de GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import LoginView from '../views/LoginView.vue'
 import AdminView from '../views/AdminView.vue'
 import PublicSiteView from '../views/PublicSiteView.vue'
 
-export const router = createRouter({ history: createWebHistory(), routes: [
+export const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes: [
   { path: '/', component: PublicSiteView, meta: { page: 'client' } },
   { path: '/site/:businessId', component: PublicSiteView, meta: { page: 'client' } },
   { path: '/login', component: LoginView, meta: { page: 'login' } },
