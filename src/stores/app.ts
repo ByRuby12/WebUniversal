cd46a919f20b7e701465944931bf9f10a9643b18
tomas <Tomascn777@gmail.com>
2026-09-23 23:22:49 +0200

    Completa sincronizacion de ajustes SEO

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -216,7 +216,6 @@ export const useAppStore = defineStore('app', () => {
     return persistPromise
   }
 
-  const persistSettings = () => saveWorkspaceSettings(settings.value, profile.value)
   const persistSettings = () => saveWorkspaceSettings(settings.value, profile.value, businesses.value)
 
   recalculateMetrics()
